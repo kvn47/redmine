24b43c964aee93f2551144dd92a3f67b06bc9b0c
kvn47 <kvn@itechdevs.com>
2014-02-27 14:13:49 +1100

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.1.0'
 
 set :application, 'redmine'
-#set :repo_url, 'git@example.com:me/my_repo.git'
+set :repo_url, 'git@github.com:kvn47/redmine.git'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
